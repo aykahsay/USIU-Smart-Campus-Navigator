0bc27474d9ec1627469c87e0ce2d077478ab653a
Ambachow Ykalom Kahsay <aykahsay19@gmail.com>
2025-09-05 03:13:23 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,13 @@
 cd "C:\Users\Admin\OneDrive - United States International University (USIU)\Desktop\USIU MAp Project"
 & "C:\Users\Admin\AppData\Local\Programs\Python\Python312\python.exe" app.py
+
+
+
+https://dashboard.ngrok.com/get-started/your-authtoken?utm_source=chatgpt.com
+
+
+
+ngrok config add-authtoken <YOUR_AUTHTOKEN>
+
+
+ngrok http --domain=myapp.ngrok-free.app 5000
